a5690e3dfde51367f1ee519d2b3a065b12a4db92
Gracjan Puch <gracjanpuszek@gmail.com>
2022-12-01 22:41:28 +0100

    Day1 converted to linq

--- Day1.cs
+++ Day1.cs
@@ -5,41 +5,21 @@ namespace AoC_2022
         public override string Part1()
         {
             var elves = input.Split("\n\n");
-            var maxCalories = 0;
-            foreach (var elf in elves)
-            {
-                var inventory = elf.Replace("\r", "").Split("\n", StringSplitOptions.RemoveEmptyEntries);
-                var calories = 0;
-                foreach (var item in inventory)
-                {
-                    calories += int.Parse(item);
-                }
-                if (maxCalories < calories)
-                    maxCalories = calories;
-            }
-
-            return maxCalories.ToString();
+            return elves.Select(x => x.Replace("\r", "").Split("\n", StringSplitOptions.RemoveEmptyEntries))
+                        .Select(x => x.Select(int.Parse).Sum())
+                        .Max()
+                        .ToString();
         }
 
         public override string Part2()
         {
-            // isTest=true;
-            // input = GetInput().Result;
             var elves = input.Split("\n\n");
-            var allCalories = new List<int>();
-            foreach (var elf in elves)
-            {
-                var inventory = elf.Replace("\r", "").Split("\n", StringSplitOptions.RemoveEmptyEntries);
-                var calories = 0;
-                foreach (var item in inventory)
-                {
-                    calories += int.Parse(item);
-                }
-                allCalories.Add(calories);
-            }
-
-            allCalories.Sort((a,b) => b > a ? 1 : -1);
-            return allCalories.Take(3).Sum().ToString();
+            return elves.Select(x => x.Replace("\r", "").Split("\n", StringSplitOptions.RemoveEmptyEntries))
+            .Select(x => x.Select(int.Parse).Sum())
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum()
+            .ToString();
         }
     }
 }
